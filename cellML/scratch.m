9cffa9e885082e9d1dd52bc955cfd16fae4c08e1
Haedong Kim <haedong.kim90@gmail.com>
2020-03-06 02:07:04 -0500

    Prototype: Modification for applying voltage clamp

---
+++ cellML/scratch.m
@@ -0,0 +1,13 @@
+clc
+close all
+clear variables
+
+
+%% run the function
+[t, S, A, C] = Rasmusson();
+
+% A = 1;
+% B = 2;
+% tspan = [0, 5];
+% y0 = [0, 0.01];
+% [t,y] = ode15s(@(t,y) odefcn(t,y,A,B), tspan, y0);
