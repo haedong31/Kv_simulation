748074a0aa0e91d4b14ed121e8f6b62a724126a5
Haedong Kim - Mac <haedong.kim90@gmail.com>
2020-03-19 12:28:45 -0400

    GA - Ito - experiment
    Do actual experiment 10 times

--- code/Ito_fit.m
+++ code/Ito_fit.m
@@ -8,7 +8,7 @@ clear variables
 K_ko = readtable("./MGAT1_Data_tidy/JMCC/K Currents 14 Weeks/potassium-KO.xlsx");
 Ito_ko = K_ko.A3FF;
 
-% voltage clamp protocol
+% voltage clamp protocol parameters
 holding_p = -70; %mV
 holding_t = 4.5; %msec
 P1 = 50; %mV
@@ -16,36 +16,38 @@ P1_t = 29.5; % msec
 P2 = 50; % mV
 P2_t = 29.5; % msec
 
-% GA
+% GA parameters
 num_vars = 6;
 lower_bd = [0,0,0,0,0,0];
 upper_bd = [60,60,60,60,60,60];
 fit_fn = @(X) Ito_fitness(X,Ito_ko,holding_p,holding_t,P1,P1_t,P2,P2_t);
-[X,fval] = ga(fit_fn,num_vars,[],[],[],[],lower_bd,upper_bd);
 
-% Compare with the Rasmusson
-[t,S,A,C] = Ito(holding_p,holding_t,P1,P1_t,P2,P2_t,X);
-[t2,S2,A2,C2] = Ito(holding_p,holding_t,P1,P1_t,P2,P2_t,[30,30,13.5,33.5,33.5,33.5]);
-
-plot(t,A(:,61),'LineWidth',2)
-hold on
-plot(t2,A2(:,61),'LineWidth',2)
-hold off
-title('I_{to} Rapidly inactivating transient outward current - KO')
-legend('GA','Rasmusson')
-xlabel('sec')
-ylabel('pA')
-
-% Compare with the real data
-fitted_Ito = A(:,61);
-fitted_Ito(end)
+% run GA
+rst = zeros(9, 8);
+for i=2:10
+    fprintf('### Iter %i / 10', i)
+    
+    [X,fval] = ga(fit_fn,num_vars,[],[],[],[],lower_bd,upper_bd);
+    
+    % run the Rasmusson with the fitted parameters
+    [~,~,A,~] = Ito(holding_p,holding_t,P1,P1_t,P2,P2_t,X);
+    
+    % fitted value (steady-state current)
+    fitted_Ito = A(:,61);
+    
+    % save the results
+    file_path = sprintf('./results/GA_Ito_ko_%i.mat', i);
+    rst(i,:) = [X, fval, fitted_Ito(end)];
+    save(file_path, 'rst');
+    disp(rst)
+end
 
 
 %% Ito - WT
 K_wt = readtable("./MGAT1_Data_tidy/JMCC/K Currents 14 Weeks/potassium-WT.xlsx");
 Ito_wt = K_wt.A3;
 
-% voltage clamp protocol
+% voltage clamp protocol parameters
 holding_p = -70; %mV
 holding_t = 4.5; %msec
 P1 = 50; %mV
@@ -53,26 +55,28 @@ P1_t = 29.5; % msec
 P2 = 50; % mV
 P2_t = 29.5; % msec
 
-% GA
+% GA parameters
 num_vars = 6;
 lower_bd = [0,0,0,0,0,0];
 upper_bd = [60,60,60,60,60,60];
 fit_fn = @(X) Ito_fitness(X,Ito_wt,holding_p,holding_t,P1,P1_t,P2,P2_t);
-[X,fval] = ga(fit_fn,num_vars,[],[],[],[],lower_bd,upper_bd);
 
-% Compare with the Rasmusson
-[t,S,A,C] = Ito(holding_p,holding_t,P1,P1_t,P2,P2_t,X);
-[t2,S2,A2,C2] = Ito(holding_p,holding_t,P1,P1_t,P2,P2_t,[30,30,13.5,33.5,33.5,33.5]);
-
-plot(t,A(:,61),'LineWidth',2)
-hold on
-plot(t2,A2(:,61),'LineWidth',2)
-hold off
-title('I_{to} Rapidly inactivating transient outward current - WT')
-legend('GA','Rasmusson')
-xlabel('sec')
-ylabel('pA')
-
-% Compare with the real data
-fitted_Ito = A(:,61);
-fitted_Ito(end)
+% run GA
+rst = zeros(10, 8);
+for i=2:10
+    fprintf('### Iter %i / 10', i)
+    
+    [X,fval] = ga(fit_fn,num_vars,[],[],[],[],lower_bd,upper_bd);
+    
+    % run the Rasmusson with the fitted parameters
+    [~,~,A,~] = Ito(holding_p,holding_t,P1,P1_t,P2,P2_t,X);
+    
+    % fitted value (steady-state current)
+    fitted_Ito = A(:,61);
+    
+    % save the results
+    file_path = sprintf('./results/GA_Ito_wt_%i.mat', i);
+    rst(i,:) = [X, fval, fitted_Ito(end)];
+    save(file_path, 'rst');
+    disp(rst)
+end
