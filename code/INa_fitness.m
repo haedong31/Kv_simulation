0bf414b85f47ac72c277c4f5815a6bab5da7eeb9
Haedong Kim - Win <haedong.kim90@gmail.com>
2020-03-23 13:57:54 -0400

    INa - intialization
    skeleton files for INa modeling

---
+++ code/INa_fitness.m
@@ -0,0 +1,5 @@
+function e = INa_fitness(X, Y, holding_p, holding_t, P1, P1_t, P2, P2_t)
+    [~,~,A,~] = Iss(holding_p, holding_t, P1, P1_t, P2, P2_t, X);
+    Iss_hat = A(:,40);
+    e = sum((Y-Iss_hat(end)).^2);
+end
