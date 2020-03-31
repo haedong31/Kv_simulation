adb8a02dff410920fb56766643d4d5b228353789
Haedong Kim - Mac <haedong.kim90@gmail.com>
2020-03-31 17:38:51 -0400

    GA - IKslow,1 - change the range 2

--- code/IKslow1_fit.m
+++ code/IKslow1_fit.m
@@ -18,8 +18,8 @@ P2_t = 29.5; % sec
 % GA parameters
 % X = [0.01176, -0.0631, 0.038198, -0.04178, 0.023391, 5.0, -0.03268, 5.0]
 num_vars = 8;
-lower_bd = [-0.01176, -0.1893, -0.038198, -0.12534, -0.023391, -5, -0.098, -5];
-upper_bd = [0.03528, 0.0631, 0.114594, 0.04178, 0.070173, 15, 0.03268, 15];
+lower_bd = [-0.1, -0.1, -0.1, -0.1, -0.1, -10, -0.1, -10];
+upper_bd = [0.1, 0.1, 0.1, 0.1, 0.1, 20, 0.1, 20];
 fit_fn = @(X) IKslow1_fitness(X,IKslow1_ko,holding_p,holding_t,P1,P1_t,P2,P2_t);
 
 % run GA
@@ -36,7 +36,7 @@ for i=1:10
     fitted_IKslow1 = A(:,67);
     
     % save the results
-    file_path = sprintf('./GA_IKslow1_ko_%i.mat', i);
+    file_path = sprintf('./GA_IKslow1_2_ko_%i.mat', i);
     rst(i,:) = [X, fval, fitted_IKslow1(end)];
     save(file_path, 'rst');
     disp(rst)
@@ -58,8 +58,8 @@ P2_t = 29.5; % sec
 % GA parameters
 % [0.01176, -0.0631, 0.038198, -0.04178, 0.023391, 5.0, -0.03268, 5.0]
 num_vars = 8;
-lower_bd = [-0.05, -0.1, -0.05, -0.05, -0.05, -5, -0.05, -5];
-upper_bd = [0.05, 0.1, 0.05, 0.05, 0.05, 15, 0.05, 15];
+llower_bd = [-0.1, -0.1, -0.1, -0.1, -0.1, -10, -0.1, -10];
+upper_bd = [0.1, 0.1, 0.1, 0.1, 0.1, 20, 0.1, 20];
 fit_fn = @(X) IKslow1_fitness(X,IKslow1_wt,holding_p,holding_t,P1,P1_t,P2,P2_t);
 
 % run GA
@@ -76,7 +76,7 @@ for i=1:10
     fitted_IKslow1 = A(:,67);
     
     % save the results
-    file_path = sprintf('./GA_IKslow1_wt_%i.mat', i);
+    file_path = sprintf('./GA_IKslow1_2_wt_%i.mat', i);
     rst(i,:) = [X, fval, fitted_IKslow1(end)];
     save(file_path, 'rst');
     disp(rst)
