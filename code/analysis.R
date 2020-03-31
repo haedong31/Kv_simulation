df203d21ff39b99a76ee40e61e1069b1126b42c9
Haedong Kim - Win <haedong.kim90@gmail.com>
2020-03-30 23:44:17 -0400

    GA - IKslow,1 - change the range
    Lower bound and upper bounds are updated

--- code/analysis.R
+++ code/analysis.R
@@ -3,6 +3,19 @@ library(readxl)
 library(ggpubr)
 
 
+## custom function -----
+data_processor <- function(excel_path, num_vars) {
+  rst <- read_excel(excel_path, col_names = FALSE)
+  for (i in 1:num_vars) {
+    colnames(rst)[i] <- str_c('x', i)
+  }
+  colnames(rst)[num_vars + 1] <- 'SSE'
+  colnames(rst)[num_vars + 2] <- 'predicted'
+  
+  return(rst)
+}
+
+
 ## data pre-processing -----
 # experimental data
 k_ko <- read_excel('./MGAT1_Data_tidy/JMCC/K Currents 14 Weeks/potassium-KO.xlsx')
@@ -13,33 +26,39 @@ k <- bind_rows(mutate(k_ko, group = 'KO'), mutate(k_wt, group = 'WT'))
 
 # results of GA
 read_names <- dir('./results/', '.*xlsx')
-ga_rsts <- vector('list', length(read_names))
-for (i in seq_along(read_names)) {
-  ga_rsts[[i]] <- read_excel(str_c('./results/', read_names[i]), col_names = FALSE)
-}
-ga_Iss_ko <- ga_rsts[[1]]
-ga_Iss_wt <- ga_rsts[[2]]
-ga_Ito_ko <- ga_rsts[[3]]
-ga_Ito_wt <- ga_rsts[[4]]
 
-colnames(ga_Iss_ko) <- c('x1', 'x2', 'x3', 'x4', 'SSE', 'Iss_hat')
-colnames(ga_Iss_wt) <- c('x1', 'x2', 'x3', 'x4', 'SSE', 'Iss_hat')
-colnames(ga_Ito_ko) <- c('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'SSE', 'Ito_hat')
-colnames(ga_Ito_wt) <- c('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'SSE', 'Ito_hat')
+ga_Iss_paths <- read_names[str_detect(read_names, 'Iss')]
+ga_Iss_paths <- str_c('./results/', ga_Iss_paths)
+ga_Ito_paths <- read_names[str_detect(read_names, 'Ito')]
+ga_Ito_paths <- str_c('./results/', ga_Ito_paths)
+ga_Ikslow1_paths <- read_names[str_detect(read_names, 'IKslow1')]
+ga_Ikslow1_paths <- str_c('./results/', ga_Ikslow1_paths)
+
+ga_Iss_ko <- data_processor(ga_Iss_paths[1], 4)
+ga_Iss_wt <- data_processor(ga_Iss_paths[2], 4)
+ga_Ito_ko <- data_processor(ga_Ito_paths[1], 6)
+ga_Ito_wt <- data_processor(ga_Ito_paths[2], 6)
+ga_Ikslow1_ko <- data_processor(ga_Ikslow1_paths[1], 8)
+ga_Ikslow1_wt <- data_processor(ga_Ikslow1_paths[2], 8)
 
 ga_Iss <- bind_rows(mutate(ga_Iss_ko, group = 'KO'), mutate(ga_Iss_wt, group = 'WT'))
 ga_Ito <- bind_rows(mutate(ga_Ito_ko, group = 'KO'), mutate(ga_Ito_wt, group = 'WT'))
+ga_Ikslow1 <- bind_rows(mutate(ga_Ikslow1_ko, group = 'KO'), mutate(ga_Ikslow1_wt, group = 'WT'))
 
 # for barplots with error bars
 k_4barplot <- k %>% 
   group_by(group) %>% 
-  summarise(mean_Iss = mean(Iss, na.rm = TRUE), mean_Ito = mean(A3, na.rm = TRUE),
-            sem_Iss = sd(Iss, na.rm = TRUE)/sqrt(n()), sem_Ito = sd(A3, na.rm = TRUE)/sqrt(n())) %>% 
+  summarise(mean_Iss = mean(Iss, na.rm = TRUE), sem_Iss = sd(Iss, na.rm = TRUE)/sqrt(n()),
+            mean_Ito = mean(A3, na.rm = TRUE), sem_Ito = sd(A3, na.rm = TRUE)/sqrt(n()),
+            mean_Ikslow1 = mean(A2, na.rm = TRUE), sem_Ikslow1 = sd(A2, na.rm = TRUE)/sqrt(n()),
+            mean_Ikslow2 = mean(A1, na.rm = TRUE), sem_Ikslow2 = sd(A1, na.rm = TRUE)/sqrt(n())) %>% 
   mutate(clf = 'Real Data')
+
 ga_Iss_4barplot <- ga_Iss %>% 
   group_by(group) %>% 
   summarise(mean_Iss = mean(Iss_hat, na.rm = TRUE), sem_Iss = sd(Iss_hat, na.rm = TRUE)/sqrt(n())) %>% 
   mutate(clf = 'Simulated')
+
 ga_Ito_4barplot <- ga_Ito %>% 
   group_by(group) %>% 
   summarise(mean_Ito = mean(Ito_hat, na.rm = TRUE), sem_Ito = sd(Ito_hat, na.rm = TRUE)/sqrt(n())) %>% 
@@ -64,6 +83,15 @@ ga_Ito %>%
   xlab('Tuning Parameters') +
   ylab('') +
   ggtitle('Ito Parameters - Genetic Algorithm') +
+  theme(plot.title = element_text(hjust = 0.5), legend.position = 'bottom')
+
+ga_Ikslow1 %>% 
+  pivot_longer(colnames(ga_Ikslow1)[1:6], names_to = 'param', values_to = 'value') %>% 
+  ggplot(aes(x = param, y = value, color = group)) +
+  geom_boxplot() +
+  xlab('Tuning Parameters') +
+  ylab('') +
+  ggtitle('Ikslow1 Parameters - Genetic Algorithm') +
   theme(plot.title = element_text(hjust = 0.5), legend.position = 'bottom')
 
 # bar plot with error bars of mean Iss & Ito
