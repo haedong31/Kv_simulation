4d8fad486110db12a000f3c1c9b476487e739c0f
Haedong Kim - Mac <haedong.kim90@gmail.com>
2020-06-12 18:02:44 +0900

    Folder organizing

--- code/Ito_fit.m
+++ code/Ito_fit.m
@@ -3,4 +3,4 @@ close all
 clear variables
 
 y = [24.8, 105.2];
-[bamp, btau, best_chroms] = custom_GA_Ito(5, y, 30, 6, 4);
+[bamp, btau, best_chroms] = Ito_AGA(5, y, 30, 6, 4);
--- code/Kv_anal_fitness.m
+++
@@ -1,6 +0,0 @@
-function z = Kv_anal_fitness(X, Ktrace, Iss)
-    t = Ktrace.time;
-    [~, peak_idx] = max(Ktrace.I);
-    IKsum = Kv_anal(t(peak_idx:end), 50, X) + Iss;
-    z = sum((IKsum - Ktrace.I(peak_idx:end)).^2);
-end
