1caf9353de4a145984e01d48b61f85bd81d26215
Haedong Kim - Win <haedong.kim90@gmail.com>
2021-05-09 12:59:29 -0400

    Recent SBGA fitting

---
+++ 20210202-SBGA-4.5s/IKslow_fit_WT.m
@@ -0,0 +1,25 @@
+%% WT
+clc
+close all
+clear variables
+format shortG
+
+% data to fit model
+y = [20.49, 1441.59];
+tol = [0.1, 5.0];
+
+% model fitting
+num_iters = 30;
+best_chroms = zeros(num_iters, 6);
+best_amps = zeros(1, num_iters);
+best_taus = zeros(1, num_iters);
+for i = 28:num_iters
+    fprintf('### Iteration %i \n', i)
+    [amps, taus, ~, chroms] = IKslow_SBGA(y, tol, 30, 6, 4);
+    best_chroms(i, :) = chroms(end, :);
+    best_amps(i) = amps(end);
+    best_taus(i) = taus(end);
+    clc
+end
+
+save('IKslow_WT.mat', 'best_chroms', 'best_amps', 'best_taus')
