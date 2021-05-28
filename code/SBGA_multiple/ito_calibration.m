3ea171a67d2f61108076630a78f0893779a85442
Haedong Kim <haedong.kim90@gmail.com>
2021-05-27 21:57:40 -0400

    Files moved

--- code/SBGA_multiple/ito_calibration.m
+++ code/SBGA_multiple/ito_calibration.m
@@ -1,3 +1,128 @@
 function [par] = ito_calibration(amp, tau, tol, N0, N1, N2)
+    % calibration arguments
+    global num_var;
+    global low;
+    global high;
+    global max_iter;
+    global window_size;
+
+    global hold_volt;
+    global hold_t;
+    global volts;
+    global pulse_t;
+    global Ek;
+
+    num_var = 6;
+    low = [0.0, 0.0, 0.0, 20.0, 2.0, 0.2];
+    high = [70.0, 70.0, 70.0, 70.0, 50.0, 0.6];
+    max_iter = 100;
+    window_size = N1; % N1 as pooling window size
+
+    hold_volt = -70;
+    hold_t = 0.125*1000;
+    volts = -50:10:50;
+    pulse_t = 4.5*1000;
+    Ek = -91.1;
+
+    % run calibration
+    best_chroms = ito_sbga(amp, tau, tol, N0, N1, N2);
+    par = best_chroms(end, :);
+end
+
+function [best_chroms] = ito_sbga(amp, tau, tol, N0, N1, N2)
+    global num_var;
+    global low;
+    global high;
+    global max_iter;
+    global window_size;
+
+    sig_list = zeros(window_size, num_var);
+end
+
+function [chroms] = init_gen(N0)
+    global num_var;
+    global low;
+    global high;
+
+    chroms = zeros(N0, num_var);
+    for j = 1:num_var
+        unif_dist = makedist('Uniform', 'lower',low(j) 'upper');
+        chroms(:, j) = random(unif_dist, N0, 1);
+    end
+end
+
+function [new_chroms] = evolve(chroms, evals, sig_list, N0, N1, N2)
+    global num_var;
+
+    % superior chromosomes 
+    [~, sup_chrom_idx] = mink(evals, N1);
+    sup_chroms = chroms(sup_chrom_idx, :);
+
+    % pooled sigma
+    sig = std(sup_chroms);
+    pooled_sig = mean(sig_list);
+
+    % mean chromosome of superiors
+    mean_elite = mean(sup_chroms, 1);
+    sup_chroms(end, :) = mean_elite;
+
+    % include superiors in new generation
+    new_chroms = zeros(N0, num_var);
+    new_chroms(1:N1, :) = sup_chroms;
     
+    % breed new chromosomes from superiors
+    cnt = 1;
+    for i = 1:N1
+        sup_chrom = sup_chroms(i, :);
+        for j = 1:N2
+            new_chrom = sup_chrom + normrnd(0, pooled_sig);
+            new_chroms((N1+cnt), :) = new_chrom;
+            cnt = cnt + 1;
+        end
+    end
+end
+
+function [amp_diff, tau_diff] = evaluation(chroms, amp, tau, N0)
+    global hold_volt;
+    global hold_t;
+    global volts;
+    global pulse_t;
+    global Ek;
+
+    num_volts = length(volts);
+
+    amp_diff = zeros(N0, num_volts);
+    tau_diff = zeros(N0, num_volts);
+    
+    for i = 1:N0
+        for j = 1:num_volts
+            volt = volts(j);
+            try
+                [t, ~, A] = Ito(chroms(i, :), hold_volt, hold_t, volt, pulse_t, Ek);
+                current_trace = A(:, 5);
+                
+                % check validity of trace shape
+                [peak, peak_idx] = max(current_trace);
+                [~, hold_idx] = min(abs(t - hold_t));
+
+                check_pt1 = any(isnan(current_trace));
+                check_pt2 = any(current_trace < 0); 
+                check_pt3 = var(current_trace(1:hold_idx)) > 1; % not stable at hold_volt
+                check_pt4 = peak_idx < hold_idx; % not stable at hold_volt of too flat at pulse
+
+                if (check_pt1 || check_pt2 || check_pt3 || check_pt4)
+                    amp_diff(i, j) = 1e+4;
+                    tau_diff(i, j) = 1e+4;
+                else
+                    % calculate discrepancies
+                    amp_diff(i, j) = abs(peak - )
+                    tau_diff(i, j) = 
+                end
+            catch
+                % large discrepancy for invalid trace shape
+                amp_diff(i, j) = 1e+4;
+                tau_diff(i, j) = 1e+4;
+            end
+        end
+    end
 end
--- code/SBGA_multiple/main.m
+++ code/SBGA_multiple/main.m
@@ -2,25 +2,19 @@ clc
 close all
 clear variables
 
-wt_data = readtable('Ik-4.5s-WT.csv');
-ko_data = readtable('Ik-4.5s-KO.csv');
+% read data
+wt_data = table2array(readtable('4.5s-avg-wt.csv'));
+[~, num_volts] = size(wt_data);
+num_volts = num_volts - 1;
 
-Ato_wt = wt_data.peak_Ito;
-tto_wt = wt_data.tau_Ito;
-Akslow_wt = wt_data.peak_IKslow;
-tkslow_wt = wt_data.tau_IKslow;
-
-Ato_ko = ko_data.peak_Ito;
-tto_ko = ko_data.tau_Ito;
-Akslow_ko = ko_data.peak_IKslow;
-tkslow_ko = ko_data.tau_IKslow;
-
+% calibration arguments
 tol = [0.1, 1.0];
 
 N0 = 30;
 N1 = 6;
 N2 = 4;
 
+% run calibration
 par_to_wt = ito_calibration(Ato_wt, tto_wt, tol, N0, N1, N2);
 par_kslow_wt = ikslow_calibration(Akslow_wt, tkslow_wt, tol, N0, N1, N2);
 
